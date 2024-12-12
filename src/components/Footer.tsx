d9a9e059f625040caf9ca9e3e984a653a0881380
phamday997 <vanday.pham@officience.com>
2024-12-12 15:10:19 +0700

    Finish style for footer and mobile for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 
 const Footer: React.FC = () => {
+  const date = new Date();
+  const currenYear = date?.getFullYear();
+
   return (
     <div className="footer-all-wraper">
-      <footer id="colophon" className="site-footer"></footer>
+      <div className="container">
+        <footer id="colophon" className="site-footer">
+          <div className="copyright">
+            <span>Developed by </span>
+            <a href="www.linkedin.com/in/phamvanday" target="_blank">
+              <strong>Phamday</strong>
+            </a>
+            <span> © {currenYear}</span>
+          </div>
+          <div className="footer-links">
+            <ul className="footer-list-links">
+              <li className="footer-list-links--item">
+                <a href="/#">Terms & Condition</a>
+              </li>
+              <li className="footer-list-links--item">
+                <a href="/#">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
