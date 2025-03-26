73a4cf2713a2d190b2a822cc5d7b8c26f1a8cbc1
phamday997 <vanday.pham@officience.com>
2025-03-26 21:28:12 +0700

    fix vite env

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const isProd = process.env.NODE_ENV === "production";
-const base = isProd ? "/Portfolio-ReactTypeScript/" : "/";
+
+const base = isProd
+  ? process.env.VITE_APP_PROD_BASE || "/Portfolio-ReactTypeScript/"
+  : process.env.VITE_APP_DEV_BASE || "/";
 
 export default defineConfig({
   plugins: [react()],
