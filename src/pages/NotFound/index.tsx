1591377d4f35c5e58be5ed53989a97a733cb4f96
phamday997 <vanday.pham@officience.com>
2025-03-27 17:21:41 +0700

    fix page 404 not show

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Icon404 from "./images/something-lost.png";
-import { Button } from "../../components";
 import "./NotFound.scss";
 import { Link } from "react-router-dom";
 
