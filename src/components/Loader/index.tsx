36af6fafaea8cdeab2b4aa856a052d411f2a2dbe
phamday997 <vanday.pham@officience.com>
2025-03-28 18:59:08 +0700

    Finish style for loading page

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./Loader.scss";
 
 export const Loader: React.FC = () => {
   return (
     <div className="overlayer-loader">
-      <div className="loader-wraper">Loadding...</div>
+      <div className="loader-wraper">
+        <span>Loading</span>
+        <span className="dot">.</span>
+        <span className="dot">.</span>
+        <span className="dot">.</span>
+      </div>
     </div>
   );
 };
